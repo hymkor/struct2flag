66dda003f9681662961736d21de6830af5756181
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-10-14 11:06:38 +0900

    Update `example2.go` for using `flag:""` on struct field

--- example2.go
+++ example2.go
@@ -9,18 +9,24 @@ import (
 	"github.com/hymkor/struct2flag"
 )
 
-type SubConfig struct {
-	Debug bool `flag:"debug,Enable debug mode"`
+type SubConfig1 struct {
+	Debug bool `flag:"d,Enable debug mode"`
+}
+
+type SubConfig2 struct {
+	Verbose bool `flag:"v,Enable verbose mode"`
 }
 
 type Config struct {
-	Name string    `flag:"name,Set your name"`
-	Sub  SubConfig // struct
+	Name string `flag:"name,Set your name"`
+	Sub1 SubConfig1
+	Sub2 SubConfig2 `flag:""`
 }
 
 func (c *Config) Run() {
 	fmt.Printf("Name=%#v\n", c.Name)
-	fmt.Printf("Sub.Debug=%#v\n", c.Sub.Debug)
+	fmt.Printf("Sub1.Debug=%#v\n", c.Sub1.Debug)
+	fmt.Printf("Sub2.Verbose=%#v\n", c.Sub2.Verbose)
 }
 
 func main() {
