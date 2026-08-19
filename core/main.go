e15453143cdf718c845de02d1f8609e9717fb3f8
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-08-19 10:46:22 +0900

    Add `core` subpackage to remove dependency on `"flag"`

---
+++ core/main.go
@@ -0,0 +1,56 @@
+package core
+
+import (
+	"reflect"
+	"strings"
+)
+
+type FlagSet interface {
+	BoolVar(p *bool, name string, value bool, usage string)
+	IntVar(p *int, name string, value int, usage string)
+	UintVar(p *uint, name string, value uint, usage string)
+	StringVar(p *string, name string, value string, usage string)
+}
+
+func BindTag(tag string, fs FlagSet, cfg interface{}) {
+	v := reflect.ValueOf(cfg).Elem()
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		field := t.Field(i)
+
+		if !field.IsExported() {
+			continue
+		}
+		desc, ok := field.Tag.Lookup(tag)
+		if !ok {
+			continue
+		}
+		switch f.Kind() {
+		case reflect.Struct:
+			BindTag(tag, fs, f.Addr().Interface())
+			continue
+		case reflect.Pointer:
+			if f.Type().Elem().Kind() == reflect.Struct && !f.IsNil() {
+				BindTag(tag, fs, f.Interface())
+				continue
+			}
+		}
+		name, usage, ok := strings.Cut(desc, ",")
+		if !ok {
+			name = strings.ToLower(field.Name)
+			usage = desc
+		}
+		switch f.Kind() {
+		case reflect.Bool:
+			fs.BoolVar(f.Addr().Interface().(*bool), name, f.Bool(), usage)
+		case reflect.Int:
+			fs.IntVar(f.Addr().Interface().(*int), name, int(f.Int()), usage)
+		case reflect.Uint:
+			fs.UintVar(f.Addr().Interface().(*uint), name, uint(f.Uint()), usage)
+		case reflect.String:
+			fs.StringVar(f.Addr().Interface().(*string), name, f.String(), usage)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,62 +2,12 @@ package struct2flag
 
 import (
 	"flag"
-	"reflect"
-	"strings"
+
+	"github.com/hymkor/struct2flag/core"
 )
 
-type FlagSet interface {
-	BoolVar(p *bool, name string, value bool, usage string)
-	IntVar(p *int, name string, value int, usage string)
-	UintVar(p *uint, name string, value uint, usage string)
-	StringVar(p *string, name string, value string, usage string)
-}
-
-func Bind(fs FlagSet, cfg interface{}) {
-	BindTag("flag", fs, cfg)
-}
-
-func BindTag(tag string, fs FlagSet, cfg interface{}) {
-	v := reflect.ValueOf(cfg).Elem()
-	t := v.Type()
-
-	for i := 0; i < v.NumField(); i++ {
-		f := v.Field(i)
-		field := t.Field(i)
-
-		if !field.IsExported() {
-			continue
-		}
-		desc, ok := field.Tag.Lookup(tag)
-		if !ok {
-			continue
-		}
-		switch f.Kind() {
-		case reflect.Struct:
-			Bind(fs, f.Addr().Interface())
-			continue
-		case reflect.Pointer:
-			if f.Type().Elem().Kind() == reflect.Struct && !f.IsNil() {
-				Bind(fs, f.Interface())
-				continue
-			}
-		}
-		name, usage, ok := strings.Cut(desc, ",")
-		if !ok {
-			name = strings.ToLower(field.Name)
-			usage = desc
-		}
-		switch f.Kind() {
-		case reflect.Bool:
-			fs.BoolVar(f.Addr().Interface().(*bool), name, f.Bool(), usage)
-		case reflect.Int:
-			fs.IntVar(f.Addr().Interface().(*int), name, int(f.Int()), usage)
-		case reflect.Uint:
-			fs.UintVar(f.Addr().Interface().(*uint), name, uint(f.Uint()), usage)
-		case reflect.String:
-			fs.StringVar(f.Addr().Interface().(*string), name, f.String(), usage)
-		}
-	}
+func Bind(fs core.FlagSet, cfg interface{}) {
+	core.BindTag("flag", fs, cfg)
 }
 
 func BindDefault(cfg interface{}) {
